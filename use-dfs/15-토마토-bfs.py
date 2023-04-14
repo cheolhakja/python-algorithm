ab90db90ab4705de6439d369265265cee355ddd7
cheolhakja <rebegin15@gmail.com>
2023-04-14 15:49:58 +0900

    15-bfs-토마토   몇개의 경로수를 거쳐가야 하나? 물어보면 DP를 고려하기
    이건 프로그래밍 구현이라기 보다 알고리즘 강의다 그러므로 동적프로그래밍 기법을 쓰는 코드가 더 도움될듯

--- use-dfs/15-토마토-bfs.py
+++ use-dfs/15-토마토-bfs.py
@@ -8,39 +8,37 @@ dy = [0, 1, 0, -1]
 if __name__ == "__main__":
     m,n = map(int, input().split())  #n이 행, m이 열
     t = [   list(  map(int, input().split())) for _ in range(n)] #t는 토마토의 약자
-    
-    day = 0
-    unrotten_num = 0
-    counting = 0
-    queue_num = 0
+    dis = [m * [0]  for _ in range(m)]
     Q = deque()
-    for i in range(n):
+    '''for i in range(n):
         for j in range(m):
             if t[i][j] == 0:
-                unrotten_num += 1 #토마토가 안익은 영역의 갯수를 샌다
-
+                unrotten_num += 1 #토마토가 안익은 영역의 갯수k를 샌다
+'''
     for i in range(n):
         for j in range(m):
             if t[i][j] == 1:
                 Q.append((i,j)) #행과 열 순서로 저장한다
-                queue_num +=1
-
-    while(counting != unrotten_num ):
-        caching = queue_num
-        queue_num = 0
-        for i in range(queue_num):
-            tmp = Q.popleft()
-            for i in range(4):
-                x = tmp[0] + dx[i]
-                y = tmp[1] + dy[i]
+                
+    while Q:
+        tmp = Q.popleft()
+        for i in range(4):           
+            x = tmp[0] + dx[i]
+            y = tmp[1] + dy[i]
+            t[x][y] = 1
+            dis[x][y] = dis[tmp[0]][tmp[1]] + 1
+            Q.append((x,y))
                 #if 인접한 토마토가 존재하고, 토마토가 사각형 범위안에 있고, 아직 안익었으면
                 #   그 토마토를 익게하고
                 #   그 토마토를 queue에 넣고
                 #   queue_num을 +=1
                 #   전체 안익은 토마토의 갯수를 +=1 해준다
-        day += 1
-        
 
-
-    #--초기화
+    max_num = -2147000
+    for i in range(n):
+        for j in range(m):
+            if t[i][j] > max_num:
+                max_num = t[i][j]
+    print(max_num)
+       
     
